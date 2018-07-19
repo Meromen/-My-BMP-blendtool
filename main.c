877d5de4f42c9fef5efe3c1a811c0d566852ed49
Meromen <32484121+Meromen@users.noreply.github.com>
2018-07-19 12:22:40 +1000

    REMOVE unnecessary fields from structs

--- main.c
+++ main.c
@@ -9,7 +9,6 @@
 typedef struct {
     uint16_t bfType; /**< BMP signature (usually 0x424D in big-endian or 0x4D42 in little-endian which means BM). */
     uint32_t bfSize; /**< BMP file size in bytes. */
-    uint16_t bfReserved1, bfReserved2; /**< Reserved fields. Usually should be 0. */
     uint32_t bfOffBits; /**< Bitmap data offset in bytes. */
 } BITMAPFILEHEADER;
 
@@ -17,18 +16,8 @@ typedef struct {
     uint32_t biSize; /**< Size of header in bytes (should be 40). */
     int32_t biWidth; /**< Width of bitmap. */
     int32_t biHeight; /**< Height of bitmap. */
-    uint16_t biPlanes; /**< Color planes. The value of this field must be 1 in BMP files. */
     uint16_t biBitCount; /**< Bits-per-pixel count. Allowed values are 1, 2, 4, 8, 16, 24, 32, 48, 64. */
-    uint32_t biCompression; /**< Compression used for bitmap data. Allowed values are stored in BITMAP_COMPRESSION enum. */
-    uint32_t biSizeImage; /**< Size of bitmap data. Can be 0 if BITMAP_COMPRESSION is 0, 3 or 6. */
-    int32_t biXPelsPerMeter; /**< Pixels-per-meter (horizontal). */
-    int32_t biYPelsPerMeter; /**< Pixels-per-meter (vertical). */
-    uint32_t biClrUsed; /**< Number of colors used in palette. */
-    uint32_t biClrImportant; /**< Number of colors used in bitmap data (from the start of palette). */
 } BITMAPINFOHEADER;
-
-
-
 
 typedef struct {
     uint8_t b; /**< A blue channel. */
@@ -47,8 +36,6 @@ typedef struct {
     uint32_t height; /**< Bitmap height. */
     BITMAP_COLOR *data; /**< Raw pixel data. */
 } BITMAP;
-
-
 
 uint32_t ReadUint32(FILE *fileS){
     uint32_t valUint32;
@@ -74,20 +61,20 @@ uint8_t ReadUint8(FILE *fileS){
     return valUint8;
 }
 
-void WriteUint32(FILE *fileS, uint32_t value){    
-    fwrite(&value, sizeof(uint32_t), 1, fileS);    
+void WriteUint32(FILE *fileS, uint32_t value){
+    fwrite(&value, sizeof(uint32_t), 1, fileS);
 }
 
-void WriteUint64(FILE *fileS, uint64_t value){    
-    fwrite(&value, sizeof(uint64_t), 1, fileS);    
+void WriteUint64(FILE *fileS, uint64_t value){
+    fwrite(&value, sizeof(uint64_t), 1, fileS);
 }
 
-void WriteUint16(FILE *fileS, uint16_t value){    
-    fwrite(&value, sizeof(uint16_t), 1, fileS);   
+void WriteUint16(FILE *fileS, uint16_t value){
+    fwrite(&value, sizeof(uint16_t), 1, fileS);
 }
 
 void WriteUint8(FILE *fileS, uint8_t value){
-    fwrite(&value, sizeof(uint8_t), 1, fileS);    
+    fwrite(&value, sizeof(uint8_t), 1, fileS);
 }
 
 uint8_t bmp_channel_blend(uint8_t a, uint8_t b, uint8_t alpha) {
@@ -140,13 +127,11 @@ BITMAP *bmp_read(const char *file) {
 
     file_header.bfType = ReadUint16(bmp_file);
     if (file_header.bfType != 0x4D42) {
-        // BM support onl
         fclose(bmp_file);
         return NULL;
     }
     file_header.bfSize = ReadUint32(bmp_file);
-    file_header.bfReserved1 = ReadUint16(bmp_file);
-    file_header.bfReserved2 = ReadUint16(bmp_file);
+    fseek(bmp_file, 4, SEEK_CUR);
     file_header.bfOffBits = ReadUint32(bmp_file);
 
     // Reading BITMAPINFO's size to detect its own version
@@ -156,9 +141,7 @@ BITMAP *bmp_read(const char *file) {
         fclose(bmp_file);
         return NULL;
     }
-
     //Reading
-
     if ((bitmapinfo_v3 = calloc(1, sizeof(BITMAPINFOHEADER))) == NULL) {
         free(bitmap);
         fclose(bmp_file);
@@ -167,16 +150,9 @@ BITMAP *bmp_read(const char *file) {
     bitmapinfo_v3->biSize = bitmapinfo_size;
     bitmapinfo_v3->biWidth =  ReadUint32(bmp_file);
     bitmapinfo_v3->biHeight = ReadUint32(bmp_file);
-    bitmapinfo_v3->biPlanes = ReadUint16(bmp_file);
+    fseek(bmp_file, 2, SEEK_CUR);
     bitmapinfo_v3->biBitCount = ReadUint16(bmp_file);
-    bitmapinfo_v3->biCompression = ReadUint32(bmp_file);
-    bitmapinfo_v3->biSizeImage = ReadUint32(bmp_file);
-    bitmapinfo_v3->biXPelsPerMeter = ReadUint32(bmp_file);
-    bitmapinfo_v3->biYPelsPerMeter = ReadUint32(bmp_file);
-    bitmapinfo_v3->biClrUsed = ReadUint32(bmp_file);
-    bitmapinfo_v3->biClrImportant = ReadUint32(bmp_file);
-
-
+    fseek(bmp_file, 24, SEEK_CUR);
     bitmap->width = bitmapinfo_v3->biWidth;
     bitmap->height = bitmapinfo_v3->biHeight;
 
@@ -189,7 +165,6 @@ BITMAP *bmp_read(const char *file) {
         fclose(bmp_file);
         return NULL;
     }
-
     // Reading "scans" as they are, bottom-to-top, left-to-right
     // TODO Microsoft says that in case of negative height the data are being read top-to-bottom
     for (int32_t y = bitmap->height - 1; y >= 0; --y) {
@@ -228,45 +203,26 @@ int bmp_write(BITMAP *bitmap, const char *file) {
     uint32_t bitmapdata_size = ((bitmap->width % 4) ?
                                 (bitmap->width + (4 - (bitmap->width % 4))) :
                                 (bitmap->width))
-                               * bitmap->height * 3;
+                                 * bitmap->height * 3;
 
 
     file_header.bfType = 0x4D42;
     file_header.bfSize = 14 + 40 + bitmapdata_size;
-    file_header.bfReserved1 = 0;
-    file_header.bfReserved2 = 0;
-    file_header.bfOffBits = 14 + 40;
 
     info_header.biSize = 40;
     info_header.biWidth = bitmap->width;
     info_header.biHeight = bitmap->height;
-    info_header.biPlanes = 1;
     info_header.biBitCount = 24;
-    info_header.biCompression = 0;
-    info_header.biSizeImage = 0;
-    info_header.biXPelsPerMeter = 0;
-    info_header.biYPelsPerMeter = 0;
-    info_header.biClrUsed = 0;
-    info_header.biClrImportant = 0;
 
     WriteUint16(bmp_file, file_header.bfType);
     WriteUint32(bmp_file, file_header.bfSize);
-    WriteUint16(bmp_file, file_header.bfReserved1);
-    WriteUint16(bmp_file, file_header.bfReserved2);
-    WriteUint32(bmp_file, file_header.bfOffBits);
-
+    fseek(bmp_file, 8 , SEEK_CUR);
     WriteUint32(bmp_file, info_header.biSize);
     WriteUint32(bmp_file, info_header.biWidth);
     WriteUint32(bmp_file, info_header.biHeight);
-    WriteUint16(bmp_file, info_header.biPlanes);
+    fseek(bmp_file, 2 , SEEK_CUR);
     WriteUint16(bmp_file, info_header.biBitCount);
-    WriteUint32(bmp_file, info_header.biCompression);
-    WriteUint32(bmp_file, info_header.biSizeImage);
-    WriteUint32(bmp_file, info_header.biXPelsPerMeter);
-    WriteUint32(bmp_file, info_header.biYPelsPerMeter);
-    WriteUint32(bmp_file, info_header.biClrUsed);
-    WriteUint32(bmp_file, info_header.biClrImportant);
-
+    fseek(bmp_file, 24 , SEEK_CUR);
 
     for (int32_t y = bitmap->height - 1; y >= 0; --y) {
         uint32_t bytes_written = 0;
@@ -291,24 +247,47 @@ int bmp_write(BITMAP *bitmap, const char *file) {
 
 
 int main(int argc, char **argv) {
-
+    char *name1;
+    char *name2;
+    char tempc[30];
     BITMAP *BMP1;
     BITMAP *BMP2;
-    BITMAP *blend;
     float temp;
     int ops;
-    printf("coefficient of opacity from 0 to 1:  ");
-    scanf("%f", &temp);
-    temp *= 255;
-    ops = (int)temp;
-
-    BMP1 = bmp_read("bmp1.bmp");
-    BMP2 = bmp_read("bmp2.bmp");
-
-    blend = bmp_combine(BMP1, BMP2, ops);
 
 
-    bmp_write(blend, "output.bmp");
+    if (argc > 1 ) {
+        name1 = argv[1];
+        BMP1 = bmp_read(name1);
+    } else {
+        printf("First bmp:  ");
+        gets(tempc);
+        BMP1 = bmp_read(tempc);
+    }
+
+
+    if (argc > 2 ) {
+        name2 = argv[2];
+        BMP2 = bmp_read(name2);
+    } else {
+        printf("Second bmp:  ");
+        gets(tempc);
+        BMP2 = bmp_read(tempc);
+    }
+
+    if (argc > 3) {
+        temp = atof(argv[3]);
+    } else {
+        printf("coefficient of opacity from 0 to 1:  ");
+        scanf("%f", &temp);
+    }
+
+    ops = ((int)(temp * 255));
+
+    BMP2 = bmp_combine(BMP1, BMP2, ops);
+
+
+    bmp_write(BMP2, "bmp2.bmp");
 
 
 
